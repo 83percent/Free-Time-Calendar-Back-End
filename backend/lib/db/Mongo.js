29d9b1d27019a8507b082b14b5e7af67fb0ad945
83Percent <77044664+83percent@users.noreply.github.com>
2021-11-19 23:13:05 +0900

    Update Mongo.js

--- backend/lib/db/Mongo.js
+++ backend/lib/db/Mongo.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 // local
 const __url = 'mongodb://127.0.0.1:27017/capstone?poolSize=20&writeConcern=majority';
 // Atlas
-//const __url = 'mongodb+srv://83percent:9J4mKCBRJy8mXweB@cluster0.ige9k.mongodb.net/Capstone?retryWrites=true&w=majority';
+//const __url = '';
 
 mongoose.connect(
     __url, 
